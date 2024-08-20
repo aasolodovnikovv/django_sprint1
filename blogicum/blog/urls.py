8e361e11d352e24c0facc8611db20367caae4ffa
Alexander Solodovnikov <aasolodovnikovv@gmail.com>
2024-08-20 13:05:49 +0300

    blogicum_ver5

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('category/<slug:category_slug>/', category_posts,
          name='category_posts'),
-    path('post/<int:post_id>/', post_detail, name='post_detail'),
+    path('posts/<int:post_id>/', post_detail, name='post_detail'),
 ]
